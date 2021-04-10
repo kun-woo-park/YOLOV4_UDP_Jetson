134b8853afe0ecddc616d519c1bc26dffc71d960
Kunwoo Park <70247684+kun-woo-park@users.noreply.github.com>
2021-04-10 17:20:03 +0900

    Update UDP_methods.py

--- UDP_jetson/UDP_methods.py
+++ UDP_jetson/UDP_methods.py
@@ -148,9 +148,9 @@ class WaveNET(nn.Module):  # define custom model named wave net, which was coine
         super(WaveNET, self).__init__()
         self.innodes = 5
 
-        self.layer1 = self._make_layer(block, planes[0], nodes[0])
-        self.layer2 = self._make_layer(block, planes[1], nodes[1])
-        self.layer3 = self._make_layer(block, planes[2], nodes[2])
+        self.layer1 = self.make_layer(block, planes[0], nodes[0])
+        self.layer2 = self.make_layer(block, planes[1], nodes[1])
+        self.layer3 = self.make_layer(block, planes[2], nodes[2])
 
         self.fin_fc = nn.Linear(self.innodes, num_classes)
 
@@ -159,7 +159,7 @@ class WaveNET(nn.Module):  # define custom model named wave net, which was coine
                 nn.init.kaiming_normal_(
                     m.weight, mode='fan_out', nonlinearity='leaky_relu')
 
-    def _make_layer(self, block, planes, nodes):
+    def make_layer(self, block, planes, nodes):
 
         layers = []
         layers.append(block(self.innodes, nodes))
@@ -169,7 +169,7 @@ class WaveNET(nn.Module):  # define custom model named wave net, which was coine
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x):
+    def forward_impl(self, x):
 
         x = self.layer1(x)
         x = self.layer2(x)
@@ -179,7 +179,7 @@ class WaveNET(nn.Module):  # define custom model named wave net, which was coine
         return x
 
     def forward(self, x):
-        return self._forward_impl(x)
+        return self.forward_impl(x)
 
 
 # sending avoidance command to Unity simulator
